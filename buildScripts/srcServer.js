324397f7fd29a22e431be9dbf65c83c2eb1c65e4
Khalil Mohammad <khalilmohammadmirza@gmail.com>
2016-11-12 22:11:46 +0500

    Added Bundling  Options for dev environment

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(require('web-pack-dev-middleware')(compiler,{
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(_dirname, '../src/index.html'));
